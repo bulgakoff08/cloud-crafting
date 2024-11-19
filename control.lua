d4a38815926e267f2953977164b3c1627cebfd4f
Mad Engineer <bulgakoff08@gmail.com>
2024-11-19 18:30:16 +0100

    v 0.0.2 / tried to make fluids

--- control.lua
+++ control.lua
@@ -13,7 +13,7 @@ end
 
 local function requestItems (context, itemId, requestAmount)
     local itemsTaken = 0
-    for _, chest in pairs(context.nonEmptyChests) do
+    for _, chest in pairs(context.chestInventories) do
         if chest.get_item_count(itemId) > 1 then
             local intention = requestAmount - itemsTaken
             itemsTaken = itemsTaken + extractFromInventory(chest, itemId, intention, true)
@@ -22,7 +22,7 @@ local function requestItems (context, itemId, requestAmount)
             end
         end
     end
-    for _, machine in pairs(context.nonEmptyMachines) do
+    for _, machine in pairs(context.outputInventories) do
         if machine.get_item_count(itemId) > 0 then
             local intention = requestAmount - itemsTaken
             itemsTaken = itemsTaken + extractFromInventory(machine, itemId, intention, false)
@@ -34,6 +34,22 @@ local function requestItems (context, itemId, requestAmount)
     return itemsTaken
 end
 
+local function requestFluid (context, fluidId, amount)
+    local fluidTaken = 0
+    for _, tank in pairs(context.nonEmptyTanks) do
+        local available = math.floor(tank.get_fluid_count(fluidId)) - 1
+        if available > 0 then
+            local toTake = math.min(amount - fluidTaken, available)
+            local taken = tank.remove_fluid({name = fluidId, amount = toTake})
+            fluidTaken = fluidTaken + taken
+            if fluidTaken >= amount then
+                return fluidTaken
+            end
+        end
+    end
+    return fluidTaken
+end
+
 local function insertItem (context, machine, itemId, count, multiplier)
     local required = count * multiplier
     local inventory = machine.get_inventory(defines.inventory.assembling_machine_input)
@@ -42,6 +58,30 @@ local function insertItem (context, machine, itemId, count, multiplier)
             local requested = requestItems(context, itemId, required)
             if requested > 0 then
                 inventory.insert({type = "item", name = itemId, count = requested})
+            end
+        end
+    end
+end
+
+local function insertFluid (context, machine, fluidId, amount)
+    local fluidInside = 0
+    for fluid, count in pairs(machine.get_fluid_contents()) do
+        if fluid == fluidId then
+            fluidInside = count
+            break
+        end
+    end
+    game.get_player(1).print("In machine: " .. fluidInside)
+    if fluidInside < amount then
+        game.get_player(1).print("Requested: " .. amount)
+        local intention = amount - fluidInside
+        game.get_player(1).print("Intention: " .. fluidId .. " " .. intention)
+        if intention > 0 then
+            local requested = requestFluid(context, fluidId, intention)
+            game.get_player(1).print("Taken from tanks: " .. fluidId .. " " .. requested)
+            if requested > 0 then
+                local inserted = machine.insert_fluid({name = fluidId, amount = requested})
+                game.get_player(1).print("Actually inserted into machine: " .. fluidId .. " " .. inserted)
             end
         end
     end
@@ -62,18 +102,21 @@ local function serveMachine (context, machine)
             if ingredient.type == "item" then
                 insertItem(context, machine, ingredient.name, ingredient.amount, craftMultiplier)
             end
+            if ingredient.type == "fluid" then
+                insertFluid(context, machine, ingredient.name, ingredient.amount)
+            end
         end
     end
 end
 
-local function serveOutput (context, output)
-    for _, item in pairs(output.get_contents()) do
+local function serveOutput (context, machine)
+    for _, item in pairs(machine.get_contents()) do
         local itemsLeft = item.count
-        for _, chest in pairs(context.nonEmptyChests) do
+        for _, chest in pairs(context.chestInventories) do
             if chest.get_item_count(item.name) > 0 then
                 local inserted = chest.insert({type = "item", name = item.name, count = item.count})
                 if inserted > 0 then
-                    itemsLeft = itemsLeft - output.remove({type = "item", name = item.name, count = inserted})
+                    itemsLeft = itemsLeft - machine.remove({ type = "item", name = item.name, count = inserted})
                 end
             end
             if itemsLeft == 0 then
@@ -83,35 +126,60 @@ local function serveOutput (context, output)
     end
 end
 
-local function createContext (chests, machines)
+local function serveFluidOutput (context, machine)
+    for fluidId, fluidCount in pairs(machine.get_fluid_contents() or {}) do
+        for _, tank in pairs(context.nonEmptyTanks) do
+            if tank.get_fluid_count(fluidId) > 0 then
+                local inserted = tank.insert_fluid({name = fluidId, amount = fluidCount})
+                if inserted > 0 then
+                    machine.remove_fluid({name = fluidId, amount = inserted})
+                    fluidCount = fluidCount - inserted
+                end
+                if fluidCount == 0 then
+                    break
+                end
+            end
+        end
+    end
+end
+
+local function createContext (chests, machines, tanks)
     local context = {
-        nonEmptyChests = {}, -- non-empty chest inventories (used to refill and output results)
-        nonEmptyMachines = {}, -- non-empty output inventories (used to refill only)
-        servedMachines = {} -- list of machines with cloud access module
+        chestInventories = {}, -- non-empty chest inventories (used to refill and output results)
+        outputInventories = {}, -- non-empty output inventories (used to refill only)
+        servedMachines = {}, -- list of machines with cloud access module
+        nonEmptyTanks = {} -- non-empty liquid tanks (used to refill and output results)
     }
     for _, chest in pairs(chests) do
         if chest and chest.valid and not chest.get_inventory(defines.inventory.chest).is_empty() then
-            table.insert(context.nonEmptyChests, chest.get_inventory(defines.inventory.chest))
+            table.insert(context.chestInventories, chest.get_inventory(defines.inventory.chest))
         end
     end
     for _, machine in pairs(machines) do
         if machine and machine.valid and machine.get_module_inventory() and machine.get_module_inventory().get_item_count("cc-cloud-access-module") > 0 then
             table.insert(context.servedMachines, machine)
             if not machine.get_output_inventory().is_empty() then
-                table.insert(context.nonEmptyMachines, machine.get_output_inventory())
+                table.insert(context.outputInventories, machine.get_output_inventory())
             end
         end
     end
+    for _, tank in pairs(tanks) do
+        if tank and tank.valid and tank.get_fluid(1) and tank.get_fluid(1).amount > 1 then
+            table.insert(context.nonEmptyTanks, tank)
+        end
+    end
+    --game.get_player(1).print("Non-empty tanks: " .. #context.nonEmptyTanks)
     return context
 end
 
 script.on_nth_tick(60, function()
     for _, surface in pairs(storage.surfaces or {}) do
-        local context = createContext(surface.chests, surface.machines)
-        for _, output in pairs(context.nonEmptyMachines) do
-            serveOutput(context, output)
+        local context = createContext(surface.chests or {}, surface.machines or {}, surface.tanks or {})
+        for _, outputInventory in pairs(context.outputInventories) do
+            serveOutput(context, outputInventory)
         end
         for _, machine in pairs(context.servedMachines) do
+            serveFluidOutput(context, machine)
             serveMachine(context, machine)
         end
     end
@@ -127,6 +195,10 @@ local function isEntityMachine (entity)
     end
 end
 
+local function isEntityTank (entity)
+    return entity.name == "cc-cloud-storage-tank"
+end
+
 local function entityPlacementHandler (entity)
     if storage.chests then
         storage.chests = nil
@@ -140,16 +212,29 @@ local function entityPlacementHandler (entity)
             storage.surfaces = {}
         end
         if storage.surfaces[surfaceIndex] == nil then
-            storage.surfaces[surfaceIndex] = {chests = {}, machines = {}}
+            storage.surfaces[surfaceIndex] = {}
         end
         if isEntityChest(entity) then
+            if not storage.surfaces[surfaceIndex].chests then
+                storage.surfaces[surfaceIndex].chests = {}
+            end
             table.insert(storage.surfaces[surfaceIndex].chests, entity)
             --game.get_player(1).print("Cloud chest registered")
             return
         end
         if isEntityMachine(entity) then
+            if not storage.surfaces[surfaceIndex].machines then
+                storage.surfaces[surfaceIndex].machines = {}
+            end
             table.insert(storage.surfaces[surfaceIndex].machines, entity)
             --game.get_player(1).print("Machine registered")
+        end
+        if isEntityTank(entity) then
+            if not storage.surfaces[surfaceIndex].tanks then
+                storage.surfaces[surfaceIndex].tanks = {}
+            end
+            table.insert(storage.surfaces[surfaceIndex].tanks, entity)
+            game.get_player(1).print("Tank registered")
         end
     end
 end
--- data.lua
+++ data.lua
@@ -36,6 +36,15 @@ data:extend({
         place_result = "cc-cloud-logistic-chest"
     },
     {
+        type = "item",
+        name = "cc-cloud-storage-tank",
+        icon = GRAPHICS_PATH .. "cc-item-cloud-storage-tank.png",
+        icon_size = 64,
+        subgroup = "storage",
+        stack_size = 50,
+        place_result = "cc-cloud-storage-tank"
+    },
+    {
         type = "module",
         name = "cc-cloud-access-module",
         icon = GRAPHICS_PATH .. "cc-item-cloud-access-module.png",
@@ -128,6 +137,21 @@ data:extend({
         },
         results = {
             {type = "item", name = "cc-cloud-logistic-chest", amount = 1}
+        }
+    },
+    {
+        type = "recipe",
+        name = "cc-cloud-storage-tank",
+        category = "crafting",
+        energy_required = 1,
+        allow_quality = false,
+        ingredients = {
+            {type = "item", name = "storage-tank", amount = 1},
+            {type = "item", name = "cc-resonating-crystal", amount = 1},
+            {type = "item", name = "pump", amount = 1}
+        },
+        results = {
+            {type = "item", name = "cc-cloud-storage-tank", amount = 1}
         }
     },
     {
@@ -234,5 +258,111 @@ data:extend({
         },
         circuit_connector = circuit_connector_definitions["chest"],
         circuit_wire_max_distance = default_circuit_wire_max_distance
+    },
+    {
+        type = "storage-tank",
+        name = "cc-cloud-storage-tank",
+        icon = GRAPHICS_PATH .. "cc-item-cloud-storage-tank.png",
+        flags = {"placeable-player", "player-creation"},
+        minable = {mining_time = 0.5, result = "cc-cloud-storage-tank"},
+        max_health = 500,
+        corpse = "storage-tank-remnants",
+        dying_explosion = "storage-tank-explosion",
+        collision_box = {{-1.3, -1.3}, {1.3, 1.3}},
+        selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+        fast_replaceable_group = "storage-tank",
+        icon_draw_specification = {scale = 1.5, shift = {0, -0.3}},
+        fluid_box = {
+            volume = 25000,
+            pipe_covers = pipecoverspictures(),
+            pipe_connections = {
+                {direction = defines.direction.north, position = {-1, -1}},
+                {direction = defines.direction.east, position = {1, 1}},
+                {direction = defines.direction.south, position = {1, 1}},
+                {direction = defines.direction.west, position = {-1, -1}}
+            },
+            hide_connection_info = true
+        },
+        two_direction_only = true,
+        window_bounding_box = {{-0.125, 0.6875}, {0.1875, 1.1875}},
+        pictures = {
+            picture = {
+                sheets = {
+                    {
+                        filename = GRAPHICS_PATH .. "cc-entity-cloud-storage-tank.png",
+                        priority = "extra-high",
+                        frames = 2,
+                        width = 219,
+                        height = 215,
+                        shift = util.by_pixel(-0.25, 3.75),
+                        scale = 0.5
+                    },
+                    {
+                        filename = "__base__/graphics/entity/storage-tank/storage-tank-shadow.png",
+                        priority = "extra-high",
+                        frames = 2,
+                        width = 291,
+                        height = 153,
+                        shift = util.by_pixel(29.75, 22.25),
+                        scale = 0.5,
+                        draw_as_shadow = true
+                    }
+                }
+            },
+            fluid_background = {
+                filename = "__base__/graphics/entity/storage-tank/fluid-background.png",
+                priority = "extra-high",
+                width = 32,
+                height = 15
+            },
+            window_background = {
+                filename = "__base__/graphics/entity/storage-tank/window-background.png",
+                priority = "extra-high",
+                width = 34,
+                height = 48,
+                scale = 0.5
+            },
+            flow_sprite = {
+                filename = "__base__/graphics/entity/pipe/fluid-flow-low-temperature.png",
+                priority = "extra-high",
+                width = 160,
+                height = 20
+            },
+            gas_flow = {
+                filename = "__base__/graphics/entity/pipe/steam.png",
+                priority = "extra-high",
+                line_length = 10,
+                width = 48,
+                height = 30,
+                frame_count = 60,
+                animation_speed = 0.25,
+                scale = 0.5
+            }
+        },
+        flow_length_in_ticks = 360,
+        impact_category = "metal-large",
+        open_sound = {filename = "__base__/sound/metallic-chest-open.ogg", volume=0.43},
+        close_sound = {filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.43},
+        working_sound = {
+            sound = {filename = "__base__/sound/storage-tank.ogg", volume = 0.6},
+            match_volume_to_activity = true,
+            audible_distance_modifier = 0.5,
+            max_sounds_per_type = 3
+        },
+        circuit_connector = circuit_connector_definitions["storage-tank"],
+        circuit_wire_max_distance = default_circuit_wire_max_distance,
+        water_reflection = {
+            pictures = {
+                filename = "__base__/graphics/entity/storage-tank/storage-tank-reflection.png",
+                priority = "extra-high",
+                width = 24,
+                height = 24,
+                shift = util.by_pixel(5, 35),
+                variation_count = 1,
+                scale = 5
+            },
+            rotate = false,
+            orientation_to_variation = false
+        }
     }
 })
